7e2f532d344ce72eb60635bb897f28b6c0ad2344
rosenrose <crazytempler@gmail.com>
2022-09-04 17:13:48 +0900

    1. 선언적 렌더링

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,38 @@
 <script>
   let name = "world!";
+  let age = 20;
+
+  setTimeout(() => {
+    [name, age] = ["foo", 30];
+  }, 2000);
+
+  const assign = () => {
+    [name, age] = ["bar", 35];
+  };
 </script>
 
-<main>
-  <h1>Hello {name}</h1>
-</main>
+<h1>Hello {name}</h1>
+<h2 class={age < 25 ? "active" : ""}>{age + name}</h2>
+<button on:click={assign}>Assign</button>
+
+<div>
+  <img src="" alt={name} />
+</div>
+<div>
+  <input type="text" bind:value={name} />
+  <input type="text" bind:value={age} />
+</div>
+
+<style>
+  h1 {
+    color: skyblue;
+  }
+
+  div {
+    margin: 1rem 0;
+  }
+
+  .active {
+    color: seagreen;
+  }
+</style>
