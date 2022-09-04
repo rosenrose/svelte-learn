3a8e895df9545e54bcee4f6e9029e88027230df5
rosenrose <crazytempler@gmail.com>
2022-09-04 16:22:57 +0900

    init

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Hello from "./Hello.svelte";
+	let name = "world";
+</script>
+
+<h1>Hello {name}!</h1>
+<button on:click={() => name = "a"}>
+	Change
+</button>
+<Hello/>
+
+<style>
+	h1 {
+		color: red;
+	}
+</style>
---
+++ src/Hello.svelte
@@ -0,0 +1,10 @@
+<h2>
+	world
+	<p>foo</p>
+</h2>
+
+<style>
+	h2 {
+		color: blue;
+	}
+</style>
