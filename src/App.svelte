1cfacefc757f076ad200c95a737871f32041c8e1
rosenrose <crazytempler@gmail.com>
2022-09-04 17:32:21 +0900

    2. 조건문과 반복문

--- src/App.svelte
+++ src/App.svelte
@@ -1,38 +1,29 @@
 <script>
   let name = "world!";
-  let age = 20;
+  let isVisible = true;
 
-  setTimeout(() => {
-    [name, age] = ["foo", 30];
-  }, 2000);
-
-  const assign = () => {
-    [name, age] = ["bar", 35];
-  };
+  let fruits = ["apple", "banana", "grape", "peach"];
 </script>
 
-<h1>Hello {name}</h1>
-<h2 class={age < 25 ? "active" : ""}>{age + name}</h2>
-<button on:click={assign}>Assign</button>
+<button on:click={() => isVisible = !isVisible}>toggle</button>
+{#if isVisible}
+  <h1>Hello {name}</h1>
+{:else}
+  <h2>No name</h2>
+{/if}
 
-<div>
-  <img src="" alt={name} />
-</div>
-<div>
-  <input type="text" bind:value={name} />
-  <input type="text" bind:value={age} />
-</div>
+<button on:click={() => fruits = fruits.slice(1)}>Eat slice</button>
+<button on:click={() => fruits.shift()}>Eat shift</button>
+<ul>
+  {#each fruits as fruit, i}
+    <li>{i} {fruit}</li>
+  {:else}
+    <p>No fruits</p>
+  {/each}
+</ul>
 
 <style>
   h1 {
     color: skyblue;
-  }
-
-  div {
-    margin: 1rem 0;
-  }
-
-  .active {
-    color: seagreen;
   }
 </style>
