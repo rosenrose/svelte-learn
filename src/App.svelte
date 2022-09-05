2e25032b0b28cfa67227b5d62d8083f5eec5722f
rosenrose <crazytempler@gmail.com>
2022-09-05 14:41:53 +0900

    4. 컴포넌트

--- src/App.svelte
+++ src/App.svelte
@@ -1,36 +1,14 @@
 <script>
-  // import { onMount } from "svelte";
-  // onMount(() => {
-  //   const box = document.querySelector(".box");
-  //   box.addEventListener("click", () => isRed = !isRed);
-  // });
+  import Fruits from "./components/Fruits.svelte";
 
-  let name = "world!";
-  let isRed = false;
-
-  let text = "text";
+  let fruits = ["apple", "banana", "grape", "peach"];
 </script>
 
-<h1>Hello {name}</h1>
-<div class="box"
-     style="--color: {isRed ? 'red' : 'seagreen'};"
-     on:click={() => isRed = !isRed}
-     on:mouseenter={() => name = "enter"}
-     on:mouseleave={() => name = "leave"}
-     >
-</div>
-
-<h2>{text}</h2>
-<!-- <input type="text" value={text} on:input={(e) => text = e.target.value} /> -->
-<input type="text" bind:value={text} />
+<Fruits {fruits} />
+<Fruits {fruits} reverse />
+<Fruits {fruits} slice="-2" />
+<Fruits {fruits} slice={[0, 3]} reverse />
+<Fruits {fruits} slice="0, 3" />
 
 <style>
-  h1 {
-    color: skyblue;
-  }
-  .box {
-    width: 15rem;
-    height: 10rem;
-    background-color: var(--color);
-  }
 </style>
---
+++ src/components/Fruits.svelte
@@ -0,0 +1,36 @@
+<script>
+  export let fruits;
+  export let reverse;
+  export let slice;
+  // console.log(reverse, slice);
+  
+  let computedFruits = fruits;
+  let name = "";
+
+  if (reverse) {
+    computedFruits = [...fruits].reverse();
+    name = "reverse";
+  }
+
+  (() => {
+    if (!slice) {
+      return;
+    }
+  
+    if (Array.isArray(slice)) {
+      computedFruits = computedFruits.slice(...slice);
+      name += ` slice ${slice.join(", ")}`;
+      return;
+    }
+  
+    computedFruits = computedFruits.slice(...slice.split(","));
+    name += ` slice ${slice}`;
+  })();
+</script>
+
+<h2>Fruits {name}</h2>
+<ul>
+  {#each computedFruits as fruit}
+    <li>{fruit}</li>
+  {/each}
+</ul>
